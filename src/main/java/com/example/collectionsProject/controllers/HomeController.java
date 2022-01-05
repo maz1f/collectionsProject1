cc0c2ffed82edec0ca7abb91445ad045183e4d27
Artem V <artem.volovich@mail.ru>
2022-01-05 21:46:20 +0300

    333

--- src/main/java/com/example/collectionsProject/controllers/HomeController.java
+++ src/main/java/com/example/collectionsProject/controllers/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -31,17 +32,22 @@ public class HomeController {
     @PostMapping("/test")
     public String delete(@RequestParam("idChecked") List<String> idrates){
 
-        if(idrates != null){
-            for(String idrateStr : idrates){
-                int idrate = Integer.parseInt(idrateStr);
 
-            }
+        if(idrates != null){
+            if (idrates.contains("1"))
+                    return "volov";
         }
         return "redirect:test";
+
     }
 
     @GetMapping("/test")
     public String test(Map<String, Object> model) {
         return "test";
+    }
+
+    @GetMapping("/volov")
+    public  String vlv() {
+        return "volov";
     }
 }
