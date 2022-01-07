67e51331f503ba548aee585fbae5b8ecc0902a97
Artem V <artem.volovich@mail.ru>
2022-01-07 14:00:19 +0300

    add bootstrap

--- src/main/java/com/example/collectionsProject/controllers/HomeController.java
+++ src/main/java/com/example/collectionsProject/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.collectionsProject.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,7 +19,7 @@ public class HomeController {
     }
 
     @GetMapping("/helloPage")
-    public String homePage(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
+    public String homePage(@RequestParam(name="name", required=false, defaultValue="unknown") String name, Map<String, Object> model) {
         model.put("user", name);
         return "helloPage";
     }
@@ -35,13 +36,15 @@ public class HomeController {
         if(idrates != null){
             if (idrates.contains("1"))
                     return "redirect:volov";
+            else return "redirect:test?message=Choose Artem Volov";
         }
         return "redirect:test";
 
     }
 
     @GetMapping("/test")
-    public String test(Map<String, Object> model) {
+    public String test(@RequestParam(name = "message", required = false, defaultValue = "") String message, Model model ) {
+        model.addAttribute("message", message);
         return "test";
     }
 
--- src/main/java/com/example/collectionsProject/controllers/UserController.java
+++ src/main/java/com/example/collectionsProject/controllers/UserController.java
@@ -6,8 +6,10 @@ import com.example.collectionsProject.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Collections;
 import java.util.Map;
@@ -20,15 +22,19 @@ public class UserController {
     private PasswordEncoder passwordEncoder;
 
     @GetMapping("/registration")
-    public String registrPage() {
+    public String registrPage(@RequestParam(name = "message", required = false, defaultValue = "") String message, Model model ) {
+
+        model.addAttribute("message", message);
         return "registration";
     }
+
+
 
     @PostMapping("/registration")
     public String registrate(User user, Map<String, Object> model) {
         User userFromDb = userRepo.findByUsername(user.getUsername());
         if (userFromDb != null) {
-            return "registration";
+            return "redirect:registration?message=This username is already exist";
         }
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setRoles(Collections.singleton(Role.USER));
@@ -38,4 +44,5 @@ public class UserController {
         model.put("users", users);
         return "redirect:login";
     }
+
 }
