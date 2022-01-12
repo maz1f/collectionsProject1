46aab9553f40e6ab749de27efb58aa61911f6ce0
Artem V <artem.volovich@mail.ru>
2022-01-12 17:28:26 +0300

    now we can delete items

---
+++ src/main/java/com/example/collectionsProject/controllers/ItemController.java
@@ -0,0 +1,36 @@
+package com.example.collectionsProject.controllers;
+
+import com.example.collectionsProject.domain.Collection;
+import com.example.collectionsProject.domain.Item;
+import com.example.collectionsProject.domain.User;
+import com.example.collectionsProject.repos.CollectionsRepo;
+import com.example.collectionsProject.repos.ItemRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ItemController {
+    @Autowired
+    private CollectionsRepo collectionsRepo;
+    @Autowired
+    private ItemRepo itemRepo;
+
+    @PreAuthorize("hasAuthority('ADMIN') or #item.collection.owner.username == authentication.name")
+    @GetMapping("/deleteItem/{item}")
+    public String deleteItem(@AuthenticationPrincipal User user, @PathVariable Item item, Model model) {
+        Collection col = item.getCollection();
+        itemRepo.delete(item);
+        col.setSize(col.getSize() - 1);
+        collectionsRepo.save(col);
+        Iterable<Item> items = itemRepo.findAllByCollection(col);
+        model.addAttribute("items", items);
+        model.addAttribute("col", col);
+        return "collection";
+    }
+}
