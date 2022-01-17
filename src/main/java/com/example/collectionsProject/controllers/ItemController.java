3b22aa5abf32020fdb2c1561efca734fb51de295
Artem V <artem.volovich@mail.ru>
2022-01-17 16:25:09 +0300

    add item editor

--- src/main/java/com/example/collectionsProject/controllers/ItemController.java
+++ src/main/java/com/example/collectionsProject/controllers/ItemController.java
@@ -10,9 +10,13 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.Map;
 
 @Controller
 public class ItemController {
@@ -23,7 +27,10 @@ public class ItemController {
 
     @PreAuthorize("hasAuthority('ADMIN') or #item.collection.owner.username == authentication.name")
     @GetMapping("/deleteItem/{item}")
-    public String deleteItem(@AuthenticationPrincipal User user, @PathVariable Item item, Model model) {
+    public String deleteItem(@AuthenticationPrincipal User user,
+                             @PathVariable Item item,
+                             Model model
+    ) {
         Collection col = item.getCollection();
         itemRepo.delete(item);
         Iterable<Item> items = itemRepo.findAllByCollection(col);
@@ -31,5 +38,37 @@ public class ItemController {
         model.addAttribute("col", col);
         model.addAttribute("item", null);
         return "redirect:/collection/" + item.getCollection().getId();
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN') or #item.collection.owner.username == authentication.name")
+    @GetMapping("/editItem/{item}")
+    public String showItemEditor(@PathVariable Item item, Model model) {
+        model.addAttribute("currentItem", item);
+        return "itemEditor";
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN') or #currentItem.collection.owner.username == authentication.name")
+    @PostMapping("/editItem/{currentItem}")
+    public String editCollection(@PathVariable Item currentItem,
+                                 @Valid Item item,
+                                 BindingResult bindingResult,
+                                 Model model
+    ) {
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorsMap = ControllerUtils.getErrors(bindingResult);
+            model.mergeAttributes(errorsMap);
+            model.addAttribute("item", item);
+            return "itemEditor";
+        } else {
+            currentItem.setName(item.getName());
+            currentItem.setTag(item.getTag());
+            itemRepo.save(currentItem);
+            model.addAttribute("col", currentItem.getCollection());
+            Iterable<Item> items = itemRepo.findAllByCollection(currentItem.getCollection());
+            model.addAttribute("items", items);
+            model.addAttribute("item", null);
+            return "redirect:/collection/" + currentItem.getCollection().getId();
+        }
+
     }
 }
