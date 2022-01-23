1a300fd2d6ca09e22089c4a2db985becf76ab39e
Artem V <artem.volovich@mail.ru>
2022-01-24 00:34:53 +0300

    add multi-language on personal page

--- src/main/java/com/example/collectionsProject/controllers/ItemController.java
+++ src/main/java/com/example/collectionsProject/controllers/ItemController.java
@@ -116,7 +116,7 @@ public class ItemController {
     @GetMapping("/{item}/comments")
     public String showComments(@PathVariable Item item, Model model) {
         model.addAttribute("item", item);
-        return "comments";
+        return "comments.ftlh";
     }
 
     @PostMapping("/comment/{item}")
